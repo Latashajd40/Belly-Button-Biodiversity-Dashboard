c4fd42f13f514eced60b1f0906313f1aa2862c79
Latashajd40 <latashajd@gmail.com>
2024-03-28 14:27:33 -0400

    completed gauge chart and updated README

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# belly-button-challenge
+# <code style="color: Darkorange">Belly Button Biodiversity Dashboard</code>
+
+## <code style="color: lightskyblue">Description</code>
+This dashboard presents findings from the 2012 study of microbial diversity in human belly buttons.  A total of 60 belly buttons were<br> 
+examined, revealing more than 70% of sampled navels harbored operational taxonomic units (OTUs).  The dashboard showcases the 10 most <br>
+prevalent OTUs found in the navel by their respective ID number, accompanied by demographic information, frequency of navel washes per week, <br>
+and the quantity of microbes found per individual.
+
+## <code style="color: lightskyblue">Example Outputs</code>
+### Top 10 OTUs found per individual <br>
+<img src="Images\barChart.png" alt="Example Image" width="500" height="300"><br><br>
+
+### The OTUs detected per individual are represented by their corresponding ID numbers <br>
+<img src="Images\bubbleChart.png" alt="Example Image"><br><br>
+
+### Demographic information for each individual categorized by ID number <br>
+<img src="Images\demo.png" alt="Example Image" width="200" height="250">
+
+### Belly Button Washing Frequency per Week <br>
+<img src="Images\bellyButtonwash.png" alt="Example Image" width="300" height="250">
+
+## <code style="color: lightskyblue">Files Included</code>
+- ### The [Images](Images) folder contains graphics of the intended output <br>
+- ### The [StarterCode](StarterCode) folder contains a nested folder [static\js](static\js):
+    * `app.js` contains the scripts for populating the dashboard graphics
+    * `index.html` creates the structure for the dashboard
+    * An example of the data from the data source is stored in `samples.json`
+
+## <code style="color: lightskyblue">Tools Used</code>
+`Javascript` , `Plotly`, `HTML`, `Bootstrap`
+
+## <code style="color: lightskyblue">Usage</code>
+### The dropdown menu operates dynamically, facilitating automatic updates to the dashboard upon selecting a new option. <br>
+<img src="Images\dropdown.png" alt="Example Image" width="150" height="75"><br><br>
+
+## <code style="color: lightskyblue">Data Source</code>
+Hulcr, J. et al. (2012) A Jungle in There: Bacteria in Belly Buttons are Highly <br>
+Diverse, but Predictable from https://robdunnlab.com/projects/belly-button-biodiversity/results-and-data/
+
+
--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -6,7 +6,12 @@ d3.json(url).then(function (data) {
     populateDropDown(data);
     let x = 0;
     initBar(data, x)
+    initBubble(data, x)
+    demoInfo(data, x)
+    speedometer(data, x)
+    console.log(data);
     return data;
+
         
        });
 
@@ -39,13 +44,14 @@ function optionChanged(selectedValue) {
 
     d3.json(url).then(function (data) {
 
-        initBar(data,i)
-   
+        initBar(data, i)
+        initBubble(data, i)
+        demoInfo(data,i)
+        speedometer(data,i)
     });
 };
 
 function initBar(data, x) {
-    d3.json(url).then(function (data) {
 
         let trace = {
        
@@ -58,12 +64,155 @@ function initBar(data, x) {
 
         let initdata = [trace];
         let layout = {
-            xaxis: data.samples[x].otu_labels
+            xaxis: data.samples[x].otu_labels,
+            height: 400,
+            width: 400,
+            margin:{'t':0,'l':150,'b':25,'r':0}
         };
         Plotly.newPlot("bar", initdata, layout);  
-    })        
+       
 };
 
+function initBubble(data,x) {
+
+    var trace1 = {
+        x: data.samples[x].otu_ids,
+        y: data.samples[x].sample_values,
+        text: data.samples[x].otu_labels,
+        mode: 'markers',
+        marker: {
+            size: data.samples[x].sample_values,
+            color: data.samples[x].otu_ids,
+            sizeref: .045,
+            sizemode: 'area'
+            
+        }
+      };
+      
+      var data = [trace1];
+      
+      var layout = {
+        showlegend: false,
+        margin:{'t':50,'l':100,'b':50,'r':0}
+      };
+      
+      Plotly.newPlot('bubble', data, layout);   
+}
+
+function demoInfo(data, x) {
+  
+    let info = data.metadata[x]
+
+    const pCard = document.getElementById("sample-metadata")
+    pCard.innerHTML = ""
+    pCard.innerHTML += `id: ${info.id}<br> ethnicity: ${info.ethnicity}<br> gender: ${info.gender}<br>
+                         age: ${info.location}<br> bbtype: ${info.bbtype}<br> wfreq: ${info.wfreq}`
+}
+
+function speedometer(data, x) {
+    
+    // console.log(data.metadata[x].wfreq);
+    if (data.metadata[x].wfreq === null) {
+        var washes = 0; 
+    } else {
+        var washes = data.metadata[x].wfreq;
+    }
+        
+
+  // Create data for the gauge chart
+var data = [{
+    type: "indicator",
+    mode: "gauge",
+    value: washes, // Set the initial value  
+    gauge: {
+        line: {width: 4},
+        axis: {range: [0, 9], ticks: "", showticklabels: false},
+        bar: {color:  'rgba(0, 0, 0, 0)', shape: {width: 2.5}},
+        bgcolor: "white",
+        borderwidth: 0,
+        steps: [
+        { range: [0, 1], color: "#f7f3eb"},
+        { range: [1, 2], color: "#f3f0e5"},
+        { range: [2, 3], color: "#e2e1c5"},
+        { range: [3, 4], color: "#e0e5af"},
+        { range: [4, 5], color: "#bbc17e"},
+        { range: [5, 6], color: "#84a07b"},
+        { range: [6, 7], color: "#87c080"},
+        { range: [7, 8], color: "#85bd8b"},
+        { range: [8, 9], color: "#61836c"}
+      ]
+    }
+    }];
+
+    let x0; // used in changing the angle of the needle
+    let y0; // used in changing the angle of the needle
+  
+
+    x0 = [.08, .08, .1, .36, .51, .65, .78, .9, .92, .92];
+    y0 = [.16, .25, .48, .7, .75, .7, .6, .45, .25, .16];
+
+    // Add labels to the gauge chart
+var labels = [
+   
+    { text: '<b>0-1<b>', showarrow: false, x: 0.02, y: .2 },
+    { text: '<b>1-2<b>', showarrow: false, x: 0.06, y: .45 },
+     { text: '<b>2-3<b>', showarrow: false, x: 0.18, y: .6 },
+    { text: '<b>3-4<b>', showarrow: false, x: 0.30, y: .77 },
+    { text: '<b>4-5<b>', showarrow: false, x: 0.52, y: .8 },
+    { text: '<b>5-6<b>', showarrow: false, x: 0.64, y: .78 },
+    { text: '<b>6-7<b>', showarrow: false, x: 0.82, y: .65 },
+    { text: '<b>7-8<b>', showarrow: false, x: 0.93, y: .45 },
+    { text: '<b>8-9<b>', showarrow: false, x: .98, y: .2 }   
+  ];
+  
+  // Add the labels to the gauge chart layout
+  gauge.layout = {
+    annotations: labels
+  };
+
+
+// Create layout options for the gauge chart
+    var layout = {
+        width: 550,
+        height: 450,
+        title: { text: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week" },
+        annotations: labels,
+                shapes: [{
+            type: 'line',
+            x0: x0[washes],
+            y0: y0[washes],
+            x1: .5,
+            y1: .15,
+            line: {
+                color: '#850000',
+                width: 4
+            }
+        },
+        
+        {
+            type: 'circle',
+            x0: .48, // x-coordinate of the circle
+            y0: 0.2, // y-coordinate of the circle (same as the end of the needle)
+            x1: 0.54, // x-radius of the circle
+            y1: 0.1, // y-radius of the circle
+            line: {
+                color: '#850000',
+                width: 2
+            },
+            fillcolor: '#850000'           
+        }
+              
+    ]
+    };
+    
+  
+  
+  // Plot the gauge chart
+  Plotly.newPlot('gauge', data, layout);
+    
+    
+
+}
 
   
 
